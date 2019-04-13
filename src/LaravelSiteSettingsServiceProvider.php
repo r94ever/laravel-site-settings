e56564fc4538bd598d0cf28ec58dde28babb8ad9
Dương Thành Văn <vandt147@outlook.com>
2019-04-13 16:49:09 +0700

    Fix issue could not publish migration

--- src/LaravelSiteSettingsServiceProvider.php
+++ src/LaravelSiteSettingsServiceProvider.php
@@ -16,7 +16,7 @@ class LaravelSiteSettingsServiceProvider extends ServiceProvider
          */
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-site-settings');
         // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-site-settings');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
